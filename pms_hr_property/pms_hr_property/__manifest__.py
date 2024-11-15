17d313d9351c336664d42aa5d002056177115370
irluidev <iramirez.spain@gmail.com>
2024-11-15 12:31:26 +0100

    [16.0][MIG]pms_hr_property: migrate from 16.0

--- pms_hr_property/pms_hr_property/__manifest__.py
+++ pms_hr_property/pms_hr_property/__manifest__.py
@@ -1,13 +1,14 @@
-# Copyright 2023 OsoTranquilo
+# Copyright from 2023 OsoTranquilo - José Luis Algara
+# Copyright 2024 Irlui Ramírez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "PMS Hr Property",
     "summary": """
         Adds to the employee the property on which he works.""",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "OsoTranquilo,Odoo Community Association (OCA)",
+    "author": "OsoTranquilo,Irluidev,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pms",
     "category": "PMS/HR",
     "depends": [
