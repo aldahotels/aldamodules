12b374d13ce372cf9f0f59ca19bad181f806e142
Miguel Granda Martínez <mgranda@comunitea.com>
2025-04-10 12:03:15 +0200

    Ver todos las solicitudes de compra

--- purchase_portal/controllers/main.py
+++ purchase_portal/controllers/main.py
@@ -66,7 +66,7 @@ class PortalAccount(CustomerPortal):
         return self._get_page_view_values(purchase_request, access_token, values, 'my_purchase_request_history', False, **kwargs)
 
     def _get_purchase_requests_domain(self):
-        return [('requested_by', '=', request.uid)]
+        return []
 
     @http.route(['/my/purchase_requests', '/my/purchase_requests/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_purchase_request(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, **kw):
