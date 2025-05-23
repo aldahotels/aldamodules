450ff9cd8543f76f2928bb1d6d9963f45ef5a350
irluidev <iramirez.spain@gmail.com>
2025-05-23 21:31:45 +0200

    16.0_[ADD]_pms_property_helpdesk_alda

---
+++ pms_property_helpdesk_alda/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Alda Hotels 2025 Jose Luis Algara Toledo
+# Copyright Alda Hotels 2025 Irlui Ramirez (irluidev)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PMS Helpdesk Alda Hotels",
+    "summary": "Create tickets related with the property",
+    "version": "16.0.0.0.0",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA),"
+    "Jose Luis Algara (Alda Hotels), Irlui Ramirez (Alda Hotels)",
+    "website": "https://github.com/OCA/pms",
+    "depends": ["helpdesk", "pms", "hr", "pms_hr_property"],
+    "category": "Generic Modules/Property Management System",
+    "data": [
+        "views/actions.xml",
+        "views/pms_property_views.xml",
+        "views/helpdesk_ticket.views.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "pms_property_helpdesk_ticket_alda/static/src/js/helpdesk_form.js"
+        ]
+    },
+    "installable": True,
+    "application": True,
+}
