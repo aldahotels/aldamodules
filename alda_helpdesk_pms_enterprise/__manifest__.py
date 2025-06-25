788f742b74536622994540a4046e5edac68ecc77
irluidev <iramirez.spain@gmail.com>
2025-06-25 17:13:06 +0200

    [ADD]16.0_helpdesk_pms_enterprise all

---
+++ alda_helpdesk_pms_enterprise/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright Alda Hotels 2025 Jose Luis Algara Toledo
+# Copyright Alda Hotels 2025 Irlui Ramirez (irluidev)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Helpdesk PMS Entreprise",
+    "summary": "Create tickets related with the property",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA),"
+    "Jose Luis Algara (Alda Hotels), Irlui Ramirez (Alda Hotels)",
+    "depends": ["helpdesk", "pms", "hr", "pms_hr_property", "mail"],
+    "website": "https://github.com/OCA/pms",
+    "category": "PMS",
+    "data": [
+        "views/actions.xml",
+        "views/pms_property_views.xml",
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "application": True,
+}
---
+++ alda_helpdesk_pms_enterprise/models/helpdesk_ticket.py
@@ -0,0 +1,70 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class HelpdeskPmsEnterprise(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    pms_property_id = fields.Many2one(
+        comodel_name="pms.property",
+        string="Property",
+        store=True,
+        required=True,
+        tracking=True,
+        help="The hotel associated with this ticket.",
+    )
+
+    pms_room_id = fields.Many2one(
+        comodel_name="pms.room",
+        string="Room",
+        domain="[('pms_property_id', '=', pms_property_id)]",
+        tracking=True,
+        help="The room associated with this ticket. It must belong to the selected property.",
+    )
+
+    @api.model
+    def fields_get(self, allfields=None, attributes=None):
+        res = super().fields_get(allfields, attributes)
+        employee = (
+            self.env["hr.employee"]
+            .sudo()
+            .search([("user_id", "=", self.env.uid)], limit=1)
+        )
+        if employee and "pms_property_id" in res:
+            res["pms_property_id"]["domain"] = [("id", "in", employee.property_ids.ids)]
+        return res
+
+    @api.onchange("pms_property_id")
+    def _onchange_pms_property(self):
+        self.pms_room_id = False
+        return {
+            "domain": {
+                "pms_room_id": [("pms_property_id", "=", self.pms_property_id.id)]
+            }
+        }
+
+    @api.constrains("pms_property_id", "pms_room_id")
+    def _check_property_assignment(self):
+        for ticket in self:
+            if ticket.pms_property_id:
+                employee = (
+                    self.env["hr.employee"]
+                    .sudo()
+                    .search([("user_id", "=", ticket.env.uid)], limit=1)
+                )
+                if employee and ticket.pms_property_id not in employee.property_ids:
+                    raise ValidationError(
+                        _("You do not have permission to assign this property.")
+                    )
+            if (
+                ticket.pms_room_id
+                and ticket.pms_room_id.pms_property_id != ticket.pms_property_id
+            ):
+                raise ValidationError(
+                    _("The selected room does not belong to the specified hotel.")
+                )
+
+            if ticket.pms_room_id and not ticket.pms_property_id:
+                raise ValidationError(
+                    _("You must select a hotel before assigning a room.")
+                )
