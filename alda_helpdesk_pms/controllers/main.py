2c795a030c72950e0f1fcd55d81c949d76771f42
irluidev <iramirez.spain@gmail.com>
2025-06-27 17:46:42 +0200

    [ADD]alda_helpdesk_pms: filter select room by room_type is overnight_room

--- alda_helpdesk_pms/controllers/main.py
+++ alda_helpdesk_pms/controllers/main.py
@@ -113,6 +113,7 @@ class HelpdeskFormController(http.Controller):
             .sudo()
             .search([("id", "=", user_id.partner_id.id)])
         )
+        
         property_record = request.env["pms.property"].sudo().browse(property_id)
         team_ids = request.env["helpdesk.team"].sudo().search([])
         ticket_type_ids = (
@@ -120,11 +121,20 @@ class HelpdeskFormController(http.Controller):
             .sudo()
             .search([("team_id", "in", team_ids.ids)])
         )
+        is_overnight_room = request.env["pms.room.type"].sudo().search([
+            ("overnight_room", "=", True)
+        ]).ids
+        _logger.debug("Overnight room types: %s", is_overnight_room)
         room_ids = (
-            request.env["pms.room"]
+            request.env["pms.room"] 
             .sudo()
-            .search([("pms_property_id", "=", property_record.id)])
+            .search([
+                ("pms_property_id", "=", property_record.id),
+                ("room_type_id", "in", is_overnight_room),
+                ("active", "=", True),
+            ])
         )
+
         location_type_options = request.env["helpdesk.ticket"]._get_location_selection()
         room_state_ids = {}
         for room in room_ids:
@@ -133,6 +143,7 @@ class HelpdeskFormController(http.Controller):
             )
 
         company_external_id = False
+
 
         return request.render(
             "alda_helpdesk_pms.create_ticket_form",
