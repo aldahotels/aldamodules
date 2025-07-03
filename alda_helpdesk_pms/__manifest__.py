517ae92b7cf35b5c154b464c4d51fd1b28d06e9e
irluidev <iramirez.spain@gmail.com>
2025-07-03 20:36:49 +0200

    [ADD]alda_helpdesk_pms: add detail pms tags, update room blocked and add traductions

--- alda_helpdesk_pms/__manifest__.py
+++ alda_helpdesk_pms/__manifest__.py
@@ -1,6 +1,23 @@
-# Copyright Alda Hotels 2025 Jose Luis Algara Toledo
-# Copyright Alda Hotels 2025 Irlui Ramirez (irluidev)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2025 Consultores Hoteleros Integrales Alda Hotels All Rights Reserved
+#    Jose Luis Algara Toledo <informatica@aldahotels.com>
+#    Irlui Tupac Ramírez Hernández <irlui@aldahotels.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     "name": "Helpdesk PMS Alda",
@@ -13,8 +30,10 @@
     "depends": ["website", "helpdesk", "alda_helpdesk_pms_enterprise"],
     "category": "PMS",
     "data": [
-        # "security/ir.model.access.csv",
-        # "views/actions.xml",
+        "security/ir.model.access.csv",
+        "views/menu_item.xml",
+        "data/ir_cron.xml",
+        "data/pms_helpdesk_ticket_data.xml",
         "views/helpdesk_ticket_type_views.xml",
         "views/helpdesk_ticket_views.xml",
         "views/helpdesk_pms_form_template.xml",
--- alda_helpdesk_pms/models/helpdesk_ticket.py
+++ alda_helpdesk_pms/models/helpdesk_ticket.py
@@ -1,8 +1,24 @@
 import json
 import os
+from random import randint
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+
+
+class HelpdeskPmsTicketDetailsTag(models.Model):
+    _name = "helpdesk.ticket.detail.tag"
+    _description = "Helpdesk PMS Ticket Details"
+    _order = "name"
+    _log_access = False
+    _auto = True
+    _transient = False
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(required=True, translate=True)
+    color = fields.Integer(default=_get_default_color)
 
 
 class HelpdeskTicket(models.Model):
@@ -15,10 +31,22 @@ class HelpdeskTicket(models.Model):
         help="The type of ticket, which determines its specific characteristics and workflow.",
     )
 
-    pms_room_blocked = fields.Boolean(
-        string="Blocked",
-        compute="_compute_pms_room_blocked",
+    tag_detail = fields.Many2many(
+        comodel_name="helpdesk.ticket.detail.tag",
+        relation="helpdesk_ticket_helpdesk_ticket_detail_tag_rel",
+        column1="ticket_id",
+        column2="detail_tag_id",
+        string="Tag Details",
+        compute="_compute_tag_detail",
+        tracking=True,
         store=True,
+    )
+
+    is_room_blocked = fields.Boolean(
+        string="Blocked",
+        compute="_compute_is_room_blocked",
+        store=True,
+        tracking=True,
         help="Indicates if the room is blocked for this ticket.",
     )
 
@@ -72,85 +100,85 @@ class HelpdeskTicket(models.Model):
         store=True,
     )
 
-    def _is_room_blocked(self, room_id):
-        reservation = (
-            self.env["pms.reservation"]
+    def _is_room_blocked(self, pms_room_id):
+        today = fields.Date.context_today(self)
+        line = (
+            self.env["pms.reservation.line"]
             .sudo()
             .search(
                 [
-                    ("reservation_type", "in", ["out"]),
-                    ("reservation_line_ids.room_id", "=", room_id),
-                    ("reservation_line_ids.date", "=", fields.Date.today()),
-                    ("reservation_line_ids.state", "=", "confirm"),
+                    ("room_id", "=", pms_room_id.id),
+                    ("date", "=", today),
+                    ("overnight_room", "=", True),
+                    ("reservation_id.state", "not in", ["draft", "cancel"]),
+                    ("reservation_id.reservation_type", "=", "out"),
                 ],
                 limit=1,
             )
         )
-        return bool(reservation)
+        return bool(line)
 
     @api.depends("pms_room_id")
-    def _compute_pms_room_blocked(self):
+    def _compute_is_room_blocked(self):
         for ticket in self:
-            ticket.pms_room_blocked = (
-                self._is_room_blocked(ticket.pms_room_id.id)
-                if ticket.pms_room_id
-                else False
-            )
+            if not ticket.pms_room_id:
+                ticket.is_room_blocked = False
+                ticket.color = 0
+            else:
+                ticket.is_room_blocked = self._is_room_blocked(ticket.pms_room_id)
+                ticket.color = 9 if ticket.is_room_blocked else 0
 
     @api.onchange("team_id")
     def _onchange_team_id_update_ticket_type_domain(self):
         result = {}
-        if self.team_id:
-            result["domain"] = {"ticket_type_id": [("team_id", "=", self.team_id.id)]}
-        else:
-            result["domain"] = {"ticket_type_id": []}
-        return result
+        for ticket in self:
+            if ticket.team_id:
+                result["domain"] = {
+                    "ticket_type_id": [("team_id", "=", ticket.team_id.id)]
+                }
+            else:
+                result["domain"] = {"ticket_type_id": []}
+            return result
 
-    @api.depends("pms_room_id")
+    @api.depends("pms_room_id", "location_type")
     def _compute_is_room(self):
         for ticket in self:
-            if ticket.is_room != bool(ticket.pms_room_id):
-                ticket.is_room = bool(ticket.pms_room_id)
-                if ticket.is_room:
-                    if ticket.location_type == "bathroom":
-                        ticket.location_type = "bathroom"
-                    else:
-                        if ticket.location_type != "room":
-                            ticket.is_room = False
-                if ticket.id and ticket.is_room:
-                    ticket.location_type = "room"
-                    msg = "Related to:" if ticket.is_room else "Unrelated to:"
-                    ticket.message_post(
-                        body=(
-                            f"{msg}</b> Room -> {ticket.pms_room_id.name} | "
-                            f"Property -> {ticket.pms_property_id.name}"
-                        ),
-                        subject=_("Room status changed"),
-                        message_type="comment",
-                        subtype_xmlid="mail.mt_comment",
-                        content_subtype="html",
-                    )
+            previous_is_room = ticket.is_room
+            current_is_room = bool(ticket.pms_room_id)
+            ticket.is_room = current_is_room
+
+            if current_is_room and ticket.location_type != "bathroom":
+                ticket.location_type = "room"
+
+            if ticket.id and previous_is_room != current_is_room:
+                msg = (
+                    _("Related to Room")
+                    if current_is_room
+                    else _("No longer related to Room")
+                )
+                room_name = ticket.pms_room_id.name if ticket.pms_room_id else _("N/A")
+                property_name = ticket.pms_property_id.name or _("N/A")
+                ticket.message_post(
+                    body=f"{msg}: {room_name} | Property: {property_name}",
+                    subject=_("Room status changed"),
+                    message_type="comment",
+                    subtype_xmlid="mail.mt_comment",
+                    content_subtype="html",
+                )
 
     @api.depends("location_type")
     def _compute_is_bathroom(self):
         for ticket in self:
-            if ticket.location_type == "bathroom":
-                ticket.is_bathroom = True
-            else:
-                ticket.is_bathroom = False
+            ticket.is_bathroom = ticket.location_type == "bathroom"
 
-    @api.onchange("pms_room_id")
-    def _onchange_pms_room_id(self):
-        pass
-
-    @api.onchange("pms_room_id")
+    @api.onchange("location_type")
     def _onchange_location_type_clear_room(self):
         for ticket in self:
             if ticket.location_type not in ["room", "bathroom"]:
-                if ticket.is_bathroom:
-                    ticket.location_type = "bathroom"
-                else:
-                    ticket.location_type = "room"
+                ticket.pms_room_id = False
+                ticket.is_bathroom = False
+                ticket.is_room = False
+                ticket.is_room_blocked = False
 
     @api.constrains("location_type", "pms_room_id")
     def _check_location_consistency(self):
@@ -165,7 +193,50 @@ class HelpdeskTicket(models.Model):
                     raise ValidationError(
                         _("Room is required when location type is 'Bathroom'")
                     )
-                if not ticket.location_type:
-                    raise ValidationError(
-                        _("Bathroom type is required when location type is 'Bathroom'")
-                    )
+
+    @api.depends("is_room", "is_room_blocked", "is_bathroom", "company_external_id")
+    def _compute_tag_detail(self):
+        for ticket in self:
+            tags = self.env["helpdesk.ticket.detail.tag"].sudo().browse()
+            if ticket.is_room:
+                tags |= self.env.ref("alda_helpdesk_pms.pms_helpdesk_ticket_is_room")
+            if ticket.is_room_blocked:
+                tags |= self.env.ref(
+                    "alda_helpdesk_pms.pms_helpdesk_ticket_is_blocked_room"
+                )
+            if ticket.is_bathroom:
+                tags |= self.env.ref("alda_helpdesk_pms.pms_helpdesk_ticket_Bathroom")
+            if ticket.company_external_id:
+                tags |= self.env.ref(
+                    "alda_helpdesk_pms.pms_helpdesk_ticket_external_company"
+                )
+            ticket.tag_detail = [(6, 0, tags.ids)]
+
+    def action_update_is_room_blocked(self):
+        tickets = self.search([("pms_room_id", "!=", False)])
+        updated_count = 0
+
+        for ticket in tickets:
+            is_blocked = self._is_room_blocked(ticket.pms_room_id)
+            if ticket.is_room_blocked != is_blocked:
+                ticket.write(
+                    {"is_room_blocked": is_blocked, "color": 9 if is_blocked else 0}
+                )
+                updated_count += 1
+
+        self.env["helpdesk.ticket"].search([])._compute_is_room_blocked()
+
+        return {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "title": "Actualización completada",
+                "message": f"{updated_count} tickets actualizados",
+                "type": "success",
+                "sticky": False,
+                "next": {
+                    "type": "ir.actions.client",
+                    "tag": "reload",
+                },
+            },
+        }
