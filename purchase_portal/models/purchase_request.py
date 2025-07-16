25cd4130340aefc3593975022ef2e9a3d0b8e81c
Vicente <vicente@comunitea.com>
2025-07-16 13:58:30 +0200

    [FIX] purchase_portal: comprobar separación líneas por hotel. Limpiar líneas sin proveedor

--- purchase_portal/models/purchase_request.py
+++ purchase_portal/models/purchase_request.py
@@ -190,13 +190,14 @@ class PurchaseRequestLine(models.Model):
 
         if lines:
             for hotel in lines.mapped('property_id'):
+                filtered_lines = lines.filtered(lambda r: r.property_id == hotel)
                 ctx = self.env.context.copy()
                 ctx['active_model'] = 'purchase.request.line'
-                ctx['active_ids'] = lines.filtered(lambda r: r.property_id == hotel).ids
-                supplier_id = lines.mapped('suggested_supplier_id')[0].id if lines.mapped('suggested_supplier_id') else \
-                    lines.mapped('supplier_id')[0].id if lines.mapped('supplier_id') else False
+                ctx['active_ids'] = filtered_lines.ids
+                supplier_id = filtered_lines.mapped('suggested_supplier_id')[0].id if filtered_lines.mapped('suggested_supplier_id') else \
+                    filtered_lines.mapped('supplier_id')[0].id if filtered_lines.mapped('supplier_id') else False
                 if not supplier_id:
-                    _logger.error(_('No supplier found for purchase request lines %s') % lines.ids)
+                    _logger.error(_('No supplier found for purchase request lines %s') % filtered_lines.ids)
                     continue
                 wiz = self.env['purchase.request.line.make.purchase.order'].with_context(ctx).create({
                     'supplier_id': supplier_id,
