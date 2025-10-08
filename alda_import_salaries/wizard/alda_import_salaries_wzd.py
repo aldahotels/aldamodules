58b5bb18b94ba90b5bccf195d577e291cfaa6c1d
Omar (Comunitea) <omar@comunitea.com>
2025-10-08 14:11:27 +0200

    [FIX] alda_import_salaries: Se estaba guardando en el asiento la referencia del asiento anterior no del actual

--- alda_import_salaries/wizard/alda_import_salaries_wzd.py
+++ alda_import_salaries/wizard/alda_import_salaries_wzd.py
@@ -60,8 +60,6 @@ class AldaImportSalariesWzd(models.TransientModel):
             if last_move_name != row[0]:
                 if last_move_name:
                     newmove_vals["line_ids"] = move_lines
-                    if len(row) > 11:
-                        newmove_vals["ref"] = row[11]
                     moves_to_create.append(newmove_vals)
                     move_lines = []
 
@@ -70,6 +68,8 @@ class AldaImportSalariesWzd(models.TransientModel):
                     "date": move_date,
                     "journal_id": self.journal_id.id,
                 }
+                if len(row) > 11:
+                    newmove_vals["ref"] = row[11]
                 last_move_name = row[0]
             if type(row[2]) == float:
                 row[2] = str(int(row[2]))
@@ -105,8 +105,6 @@ class AldaImportSalariesWzd(models.TransientModel):
 
             if rownum == sheet.nrows - 1:
                 newmove_vals["line_ids"] = move_lines
-                if len(row) > 11:
-                    newmove_vals["ref"] = row[11]
                 moves_to_create.append(newmove_vals)
 
         if moves_to_create:
