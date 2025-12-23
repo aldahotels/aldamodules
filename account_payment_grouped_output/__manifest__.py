cb7da0606832c3f6e25cdaf629a5ab5f9066eed4
Omar (Comunitea) <omar@comunitea.com>
2025-12-23 18:52:26 +0100

    [ADD] account_payment_grouped_output: Allows group pending payments by date and journal automatically

---
+++ account_payment_grouped_output/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Payment - Generate grouped moves",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Comunitea",
+    "website": "https://www.comunitea.com",
+    "category": "Banking addons",
+    "depends": ["account"],
+    "data": ["data/ir_cron.xml",
+             "views/account_journal_view.xml"],
+    "installable": True,
+}
